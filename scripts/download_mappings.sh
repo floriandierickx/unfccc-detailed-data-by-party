d1d8c52819f004447a96470c1c0d13af19559da7
Florian Dierickx <floriandierickx@gmail.com>
2020-10-18 17:10:07 +0200

    Update download_mappings.sh
    addition of all GET api json files from 'user defined indicators' (AnnexOne and nonAnnexOne)

--- scripts/download_mappings.sh
+++ scripts/download_mappings.sh
@@ -13,17 +13,32 @@
 # };
 
 cd `dirname $0`/api-mappings
+
+# @FlorianDRX - extended with extra GET sequences at https://di.unfccc.int/indicators_annex1 (obtain with 'inspect element' -> method = GET -> 'headers' in Firefox)
 wget http://di.unfccc.int/api/parties -q -O- | \
   python3 -m json.tool > parties.json
 wget http://di.unfccc.int/api/years/groups -q -O- | \
   python3 -m json.tool > years.json
 wget http://di.unfccc.int/api/dimension-instances/category -q -O- | \
   python3 -m json.tool > categories.json
-wget http://di.unfccc.int/api/dimension-instances/gas -q -O- | \
-  python3 -m json.tool >  gases.json
 wget http://di.unfccc.int/api/dimension-instances/classification -q -O- | \
-  python3 -m json.tool >  classifications.json
+  python3 -m json.tool > classifications.json  # @FlorianDRX - 2nd GET sequence
+wget https://di.unfccc.int/api/dimension-instances/measure -q -0- | \ 
+  python3 -m json.tool > measures.json # @FlorianDRX - 3rd GET sequence
+wget http://di.unfccc.int/api/dimension-instances/gas -q -O- | \
+  python3 -m json.tool > gases.json
 wget http://di.unfccc.int/api/conversion -q -O- | \
-  python3 -m json.tool > conversion.json
+  python3 -m json.tool > conversion.json # @FlorianDRX - has 3 objects (annexOne [1152], nonAnnexOne [956] and units [16])
+wget http://di.unfccc.int/api/conversion/fq/ -q -O- | \
+  python3 -m json.tool > conversion2.json # @FlorianDRX - has 3 objects (annexOne [2738], nonAnnexOne [2891] and units [179])
+
+# @FlorianDRX: other API GET sequences used to check existence of variable combination pairs (?)
+## AnnexOne:
+wget https://di.unfccc.int/api/variables/fq/annexOne -q -O- | \
+  python3 -m json.tool > variables_AnnexOne.json
+## NonAnnexOne:
+wget https://di.unfccc.int/api/variables/fq/nonAnnexOne -q -O- | \
+  python3 -m json.tool > variables_nonAnnexOne.json
+
 pwd
 ls -l
